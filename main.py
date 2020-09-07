53c01e0dba43d8de640e72073b2751c1549372fb
Vincent Uden <vincent.uden@itggot.se>
2020-09-07 14:48:20 +0200

    curl

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 
-def divergence(phi):
-    grad = np.gradient(phi)
+def divergence(u, v):
+    grad_u = np.gradient(u)
+    grad_v = np.gradient(v)
 
-    grad_x = np.gradient(grad[0])
-    grad_y = np.gradient(grad[1])
-
-    div =  grad_x[0] + grad_y[1]
+    div =  grad_u[0] + grad_v[1]
 
     return div
+
+def curl(u, v):
+    grad_u = np.gradient(u)
+    grad_v = np.gradient(v)
+
+    curl = grad_v[0] - grad_u[1]
+
+    return curl
 
 P = np.loadtxt('tryckfalt.dat')
 u = np.loadtxt('vindfalt_u.dat')
@@ -20,7 +26,8 @@ x = np.linspace(0, 55*30, 31)
 y = np.linspace(0, 55*29, 30)
 
 grad = np.gradient(P)
-div = divergence(P)
+div = divergence(u, v)
+crl = curl(u, v)
 
 fig, ax = plt.subplots(2,2)
 
@@ -28,14 +35,15 @@ c = ax[0,0].contourf(x, y, P, 11)
 plt.colorbar(c, ax=ax[0,0])
 ax[0,0].set_title("Nivåkurvor, lufttryck (hPa)")
 
-ax[0,1].streamplot(x, y, u, v, linewidth=0.7)
+ax[0,1].streamplot(x, y, u, v, linewidth=0.7, density=2)
 ax[0,1].set_title("Fältlinjer, vindhastighet")
 
 c = ax[1,0].contourf(x,y,div)
 plt.colorbar(c, ax=ax[1,0])
-ax[1,0].set_title("Divergens, lufttryck")
+ax[1,0].set_title("Divergens, vindhastighet")
 
-ax[1,1].quiver(x,y,grad[0],grad[1])
-ax[1,1].set_title("Gradient, lufttryck")
+c = ax[1,1].contourf(x,y,crl)
+plt.colorbar(c, ax=ax[1,1])
+ax[1,1].set_title("Rotation, vindhastighet")
 
 plt.show()
